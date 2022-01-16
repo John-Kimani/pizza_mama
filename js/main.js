f1c96ba905677f2fee7cc62184879941f6d703c2
John-Kimani <kimwaweru100@gmail.com>
2022-01-16 17:51:17 +0300

    creates add to cart button loop and variable

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 // console.log("hello") //confirm link
+// var and loops start
+
 //remove cart button loop start
 var removeCartItemButtons = document.getElementsByClassName('btn-danger')
 console.log(removeCartItemButtons) //confirms button selects
@@ -8,13 +10,27 @@ for(var i=0; i < removeCartItemButtons.length; i++){
     button.addEventListener('click', removeCartItem)
 }
 // remove cart  button loop end
-
+//  set input value loop
 var quantityInputs = document.getElementsByClassName('ngapi')
 for(var i=0; i < quantityInputs.length; i++){
     var input = quantityInputs[i]
     input.addEventListener('change', quantityChanged)
 }
-// functions
+// set input value loop end
+// add to cart loop start
+var addToCartButtons = document.getElementsByClassName('add-to-cart')
+for(var i=0; i < quantityInputs.length; i++){
+    var button = addToCartButtons[i]
+    button.addEventListener('click', addToCartClicked)
+}
+
+// add to cart loop end
+
+// loops end
+
+// var and loops end
+
+// functions starts
 // remove cart button function
 function removeCartItem(event){
     var buttonClicked = event.target
@@ -32,6 +48,12 @@ function quantityChanged(event){
     updateCartTotal()
 }
 //set input value function end
+
+// add to cart function start
+function addToCartClicked(){
+    
+}
+// add to cart function end
 
 //function to cart item price total
 
@@ -51,7 +73,7 @@ function updateCartTotal(){
         // console.log(price * quantity) //display math function
         total = total + (price * quantity) //new total for every cart item
     }
-    total = Math.round(total * 100)/ 100
+    total = Math.round(total * 100)/ 100 //sets total to the nearest 2 decimal place
     document.getElementsByClassName('cart-total-price')[0].innerText = 'ksh.' + total //sets and inserts new total in html
 
 }
