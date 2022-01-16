2f63ddc2f064e1254ef16381f21c6fc954ffe2d3
John-Kimani <kimwaweru100@gmail.com>
2022-01-16 16:32:19 +0300

    creates update cart total function and fetch input data from html

--- js/main.js
+++ js/main.js
@@ -6,9 +6,26 @@ console.log(removeCartItemButtons) //confirms button selects
 for(var i=0; i < removeCartItemButtons.length; i++){
     var button = removeCartItemButtons[i]
     button.addEventListener('click', function(event){
+        // console.log("Clicked") //confirms event listener
         var buttonClicked = event.target
         buttonClicked.parentElement.parentElement.remove()
+        updateCartTotal()
     })
 }
 
 //remove cart button function end
+
+//function to cart item price total
+
+function updateCartTotal(){
+    var cartItemForm = document.getElementsByClassName('cart-items')[0]
+    // fetch objects inside class
+    var cartRows = cartItemForm.getElementsByClassName('cart-row')
+    for(var i=0; i < cartRows.length; i++){
+        var cartRow = cartRows[i]
+        var bei = cartRow.getElementsByClassName('bei')[0]
+        var ngapi = cartRow.getElementsByClassName('ngapi')[0]
+        console.log(bei, ngapi)
+    }
+
+}
